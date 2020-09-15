81c8f79951e149f1108c15a19f7f521d9ca439c8
kicok <kicoker@gmail.com>
2020-09-15 10:53:47 +0900

    add category

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import './dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category-meals';
@@ -14,14 +14,18 @@ class CategoryMealsScreen extends StatelessWidget {
         ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'];
     final CategoryId = routeArgs['id'];
+    final catetegoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(CategoryId);
+    }).toList();
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body: Center(
-        child: Text(
-          'The Recipes For The Category!',
-        ),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Text(catetegoryMeals[index].title);
+        },
+        itemCount: catetegoryMeals.length,
       ),
     );
   }
